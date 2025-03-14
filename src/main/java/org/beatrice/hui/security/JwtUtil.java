f155ab487e896f5960e7dc549d2468d8ec8a0ed1
BeatriceLaplan <beatricelaplan@gmail.com>
2025-03-15 00:28:14 +0300

    qwerty

--- src/main/java/org/beatrice/hui/security/JwtUtil.java
+++ src/main/java/org/beatrice/hui/security/JwtUtil.java
@@ -1,5 +1,7 @@
 package org.beatrice.hui.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
@@ -9,7 +11,7 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 
 public class JwtUtil {
-    private static String SECRET_KEY;
+    private static SecretKey SECRET_KEY;
     private static final long EXPIRATION_DATE = 1000 * 60 * 60; // 1 hour
 
     @Value("${jwt.secret}")
@@ -17,17 +19,23 @@ public class JwtUtil {
 
     @PostConstruct
     public void init() {
-        SECRET_KEY = secret;
+        SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public static String generateToken(String email) {
-        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_DATE))
-                .signWith(key)
+                .signWith(SECRET_KEY)
                 .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith()
+        }
     }
 }
