2288b031a7d74bf1519b71531e362efd4982ef8f
Beatrice <btrc0936@gmail.com>
2025-03-21 15:41:08 +0300

    added dto for task requests

---
+++ src/main/java/org/beatrice/dgtuProject/dto/TaskRequest.java
@@ -0,0 +1,54 @@
+package org.beatrice.dgtuProject.dto;
+
+import org.beatrice.dgtuProject.model.Tag;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class TaskRequest {
+    private String name;
+    private String description;
+    private LocalDateTime deadline;
+    private String status;
+    private Set<Tag> tags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+}
