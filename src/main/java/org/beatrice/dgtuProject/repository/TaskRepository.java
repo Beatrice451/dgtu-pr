0b347197882a85e051de00674054cb4bed2f5292
Beatrice <btrc0936@gmail.com>
2025-03-31 11:17:52 +0300

    qwerty

--- src/main/java/org/beatrice/dgtuProject/repository/TaskRepository.java
+++ src/main/java/org/beatrice/dgtuProject/repository/TaskRepository.java
@@ -2,9 +2,10 @@ package org.beatrice.dgtuProject.repository;
 
 import org.beatrice.dgtuProject.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     List<Task> findAllByUserEmail(String userEmail);
 }
---
+++ src/main/java/org/beatrice/dgtuProject/specification/TaskSpecifications.java
@@ -0,0 +1,25 @@
+package org.beatrice.dgtuProject.specification;
+
+
+import org.beatrice.dgtuProject.model.Task;
+import org.springframework.data.jpa.domain.Specification;
+
+public class TaskSpecifications {
+    public static Specification<Task> hasStatus(String status) {
+        return ((root, query, criteriaBuilder) ->
+            criteriaBuilder.equal(root.get("status"), status)
+        );
+    }
+
+    public static Specification<Task> hasPriority(String priority) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("priority"), priority)
+        );
+    }
+
+    public static Specification<Task> hasNameContaining(String name) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("name"), "%" + name + "%")
+        );
+    }
+}
